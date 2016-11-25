aaa225155df8473c07f5100c9d6608af01cc5e27
therrance <lozuvatka@gmail.com>
2016-11-25 12:55:05 +0000

    populating templateCach directly

--- file.js
+++ file.js
@@ -1,19 +1,25 @@
 /*global angular*/
 
-(function(){
-    function TestCtrl($templateCache){
-        this.user = {name: 'Blake'};
-        
+(function() {
+    function TestCtrl($templateCache) {
+        this.user = {
+            name: 'Blake'
+        };
+
         console.log($templateCache.get('test.html'));
     }
-    
+
     angular.module('app', ['ngRoute'])
-    .config(function($routeProvider){
-        $routeProvider.when('/', {
-            controller: 'TestCtrl as test',
-            templateUrl: 'test.html'
+        .config(function($routeProvider) {
+            $routeProvider.when('/', {
+                    controller: 'TestCtrl as test',
+                    templateUrl: 'test.html'
+                })
+                .otherwise('/');
         })
-        .otherwise('/');
-    })
-    .controller('TestCtrl', TestCtrl);
+        .controller('TestCtrl', TestCtrl);
+
+    angular.module('app').run(function($templateCache) {
+        $templateCache.put('test.html', 'Hello {{ test.user.name }}!');
+    });
 })();
